d4b5849b5d0854feea880b1c2dfd2cf3209e0f39
Matt Van Horn <mattvanhorn@gmail.com>
2013-09-23 10:29:00 -0700

    Stock Rails 4 app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PizzaBeer::Application.config.secret_key_base = '48d1da0755914e72292d71770cc12f2ee4189e73f82bd6f6902fc202fa6d49af69a873428bdb9955dbe03b5efdd50bba7bf48481e803f3f733e762738ab73689'

29a2a028304d5f2d8b1ed7317b5e8ee4e65e5f77
Matt Van Horn <mattvanhorn@gmail.com>
2013-09-24 10:30:17 -0700

    Interface changes

--- features/support/page_objects/estimates_pages.rb
+++ features/support/page_objects/estimates_pages.rb
@@ -1,11 +1,12 @@
 class NewEstimatePage < SitePrism::Page
   URL = Rails.application.routes.url_helpers.root_path
+
   set_url URL
   set_url_matcher %r(#{URL})
 
   element :guests_field, "input[name='estimate[guest_count]']"
-  element :slices_field, "input[name='estimate[slice_count]']"
-  element :beers_field, "input[name='estimate[beer_count]']"
+  element :hunger_select, "select[name='estimate[slice_count]']"
+  element :thirst_select, "select[name='estimate[beer_count]']"
   element :submit_btn, "input[name='commit']"
 
   def guests_expected=(guest_count)
@@ -15,22 +16,16 @@ class NewEstimatePage < SitePrism::Page
 
   def hunger_level=(hunger)
     load unless displayed?
-    slices_per_person = case hunger
-      when 'full'     then 1
-      when 'hungry'   then 2
-      when 'starving' then 3
-    end
-    slices_field.set slices_per_person
+    hunger_select.select hunger
   end
 
   def thirst_level=(thirst)
     load unless displayed?
-    beer_count = case thirst
-      when 'none' then 0
-      when 'like' then 2
-      when 'love' then 3
+    thirst_select.select case thirst
+      when 'like' then 'thirsty'
+      when 'love' then 'extremely thirsty'
+      else 'none'
     end
-    beers_field.set beer_count
   end
 
   def request_estimate
